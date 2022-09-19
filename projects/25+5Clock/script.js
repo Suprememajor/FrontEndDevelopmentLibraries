c3f61c7577946bdeb4fa6e4dca3568e120c819e7
Suprememajor <ndipnobertetta@gmail.com>
2022-09-19 20:02:08 +0100

    added session reduce and increase fxns

--- projects/25+5Clock/script.js
+++ projects/25+5Clock/script.js
@@ -6,3 +6,11 @@ const Decreasebreak = () => {
     let breakLength = document.getElementById("break-length");
     breakLength.innerHTML = parseInt(breakLength.innerHTML) + 1;
   }
+  const DecreaseSession = () => {
+    let sessionLength = document.getElementById("session-length");
+    sessionLength.innerHTML = parseInt(sessionLength.innerHTML) - 1;
+  }
+  const IncreaseSession = () => {
+    let sessionLength = document.getElementById("session-length");
+    sessionLength.innerHTML = parseInt(sessionLength.innerHTML) + 1;
+  }
