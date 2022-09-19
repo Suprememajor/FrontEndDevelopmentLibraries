a0cf45ab717385cde86414e0424ac784e90b1e1a
Suprememajor <ndipnobertetta@gmail.com>
2022-09-19 20:14:37 +0100

    added time-left control using reduce and increase btns

--- projects/25+5Clock/script.js
+++ projects/25+5Clock/script.js
@@ -1,16 +1,36 @@
-const Decreasebreak = () => {
-    let breakLength = document.getElementById("break-length");
-    breakLength.innerHTML = parseInt(breakLength.innerHTML) - 1;
+let resetBtn = document.getElementById("reset");
+let timerLabel = document.getElementById("timer-label");
+let timerLeft = document.getElementById("time-left");
+let sessionLength = document.getElementById("session-length");
+let breakLength = document.getElementById("break-length");
+  
+
+let reset = () => {
+  document.getElementById("break-length").innerHTML = 5;
+  document.getElementById("session-length").innerHTML = 25;
+  //document.getElementById("time-left").innerHTML = "25:00";
+}
+
+const Decreasebreak = () => {  
+  breakLength.innerHTML = parseInt(breakLength.innerHTML) - 1;
+  updateTimeleft();
+}
+const Increasebreak = () => {
+  breakLength.innerHTML = parseInt(breakLength.innerHTML) + 1;
+  updateTimeleft();
+}
+const DecreaseSession = () => {
+  sessionLength.innerHTML = parseInt(sessionLength.innerHTML) - 1;
+  updateTimeleft();
+}
+const IncreaseSession = () => {
+  sessionLength.innerHTML = parseInt(sessionLength.innerHTML) + 1; 
+  updateTimeleft();
+}
+const updateTimeleft = () => {
+  if(timerLabel.innerHTML === "Session") {
+    timerLeft.innerHTML = sessionLength.innerHTML + ":00";
+  } else {
+    timerLeft.innerHTML = breakLength.innerHTML + ":00";
   }
-  const Increasebreak = () => {
-    let breakLength = document.getElementById("break-length");
-    breakLength.innerHTML = parseInt(breakLength.innerHTML) + 1;
-  }
-  const DecreaseSession = () => {
-    let sessionLength = document.getElementById("session-length");
-    sessionLength.innerHTML = parseInt(sessionLength.innerHTML) - 1;
-  }
-  const IncreaseSession = () => {
-    let sessionLength = document.getElementById("session-length");
-    sessionLength.innerHTML = parseInt(sessionLength.innerHTML) + 1;
-  }
+}
