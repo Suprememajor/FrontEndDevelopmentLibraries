3978b9558bf6deedf0d2243dfea61c5ee7087bf5
Suprememajor <ndipnobertetta@gmail.com>
2022-09-20 02:52:37 +0100

    project 4 User Story #8 complete

--- projects/JavaScriptCalculator/script.js
+++ projects/JavaScriptCalculator/script.js
@@ -1,3 +1,13 @@
+let expression = "0";
 let clearDisp = () => {    
     document.getElementById("display").innerHTML = 0;
+    expression = "0";
+}
+
+let getInput = (number) => {
+    expression += number;
+    while(expression[0] === "0" && expression.length > 1) {
+        expression = expression.substring(1);
+    }
+    document.getElementById("display").innerHTML = expression;
 }
