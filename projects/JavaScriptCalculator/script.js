c1769ac595b8bc6e94435e850932ec09f532ab9b
Suprememajor <ndipnobertetta@gmail.com>
2022-09-20 03:54:18 +0100

    project 4 User Story #13 complete

--- projects/JavaScriptCalculator/script.js
+++ projects/JavaScriptCalculator/script.js
@@ -1,5 +1,6 @@
 let expression = "0";
 var safe = true;
+var signed = false;
 let clearDisp = () => {
     document.getElementById("display").innerHTML = 0;
     expression = "0";
@@ -13,8 +14,13 @@ let getInput = (element) => {
         else
             safe = false;
     if(element === "-" || element === "+" || 
-       element === "*" || element === "/")
+       element === "*" || element === "/") {
         safe = true;
+        if(element != "-" && signed) {
+            removeOperators();
+        }
+        signed = true;
+    } else if(element >= 0 && element <= 9) signed = false;
     expression += element;
     while (expression[0] === "0" && expression.length > 1) {
         expression = expression.substring(1);
@@ -26,4 +32,13 @@ let compute = () => {
     document.getElementById("display").innerHTML = eval(expression);
     expression = "0";
     safe = true;
+}
+
+let removeOperators = () => {
+    while(expression[expression.length - 1] === "+" ||
+         expression[expression.length - 1] === "-" ||
+         expression[expression.length - 1] === "*" ||
+         expression[expression.length - 1] === "/") {
+        expression = expression.substring(0, expression.length - 1);
+    }        
 }
